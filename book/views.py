a556bc596ad62edc17aadbb8162cbd885706dbf3
Viktor Velychkocd <velviktor831@gmail.com>
2026-05-17 12:54:36 +0300

    Implement Book model and CRUD API with permissions

---
+++ book/views.py
@@ -0,0 +1,14 @@
+from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser, AllowAny
+from .models import Book
+from .serializers import BookSerializer
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    def get_permissions(self):
+        if self.action in ["list", "retrieve"]:
+            return [AllowAny()]
+        return [IsAdminUser()]
