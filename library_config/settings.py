fe612452be820aeb28e34e3752476049ab055718
Viktor Velychkocd <velviktor831@gmail.com>
2026-05-17 14:17:16 +0300

    finalize User service

--- library_config/settings.py
+++ library_config/settings.py
@@ -41,6 +41,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "user",
+    "book",
 ]
 
 MIDDLEWARE = [
@@ -125,3 +128,11 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "user.User"
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+}
